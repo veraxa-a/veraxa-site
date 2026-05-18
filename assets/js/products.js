c63c1a8d987826367503635360a39c47b8b0f6f1
veraxa-a <veraxa.contact@gmail.com>
2026-05-19 02:59:04 +0300

    Update product list with new items and details

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,23 +1,66 @@
 const VERAXA_PRODUCTS = [
   {
-    name: "VÉRAXA Oversize Tee",
-    price: "1.290₺",
-    category: "tshirt",
-    image: "assets/images/products/product-1.jpg",
-    whatsappText: "VÉRAXA Oversize Tee"
+    name: "VÉRAXA Mocha Sleeveless Top",
+    price: "899₺",
+    category: "top",
+    image: "assets/images/products/photo_5789718002545659185_y.jpg",
+    hoverImage: "assets/images/products/photo_5789718002545659186_y.jpg",
+    whatsappText: "VÉRAXA Mocha Sleeveless Top"
   },
   {
-    name: "VÉRAXA Shadow Hoodie",
-    price: "2.490₺",
-    category: "hoodie",
-    image: "assets/images/products/product-2.jpg",
-    whatsappText: "VÉRAXA Shadow Hoodie"
+    name: "VÉRAXA White Sleeveless Top",
+    price: "899₺",
+    category: "top",
+    image: "assets/images/products/photo_5789718002545659189_y (1).jpg",
+    hoverImage: "assets/images/products/photo_5789718002545659190_y.jpg",
+    whatsappText: "VÉRAXA White Sleeveless Top"
   },
   {
-    name: "VÉRAXA Essential Black",
-    price: "1.590₺",
-    category: "tshirt",
-    image: "assets/images/products/product-3.jpg",
-    whatsappText: "VÉRAXA Essential Black"
+    name: "VÉRAXA Black Sleeveless Top",
+    price: "899₺",
+    category: "top",
+    image: "assets/images/products/photo_5789718002545659195_y.jpg",
+    hoverImage: "assets/images/products/photo_5789718002545659196_y.jpg",
+    whatsappText: "VÉRAXA Black Sleeveless Top"
+  },
+  {
+    name: "VÉRAXA Grey Crop Top",
+    price: "849₺",
+    category: "crop",
+    image: "assets/images/products/photo_5823488260943498600_y.jpg",
+    hoverImage: "assets/images/products/photo_5823488260943498603_y.jpg",
+    whatsappText: "VÉRAXA Grey Crop Top"
+  },
+  {
+    name: "VÉRAXA Green Crop Top",
+    price: "849₺",
+    category: "crop",
+    image: "assets/images/products/photo_5823488260943498605_y.jpg",
+    hoverImage: "assets/images/products/photo_5823488260943498607_y.jpg",
+    whatsappText: "VÉRAXA Green Crop Top"
+  },
+  {
+    name: "VÉRAXA Black Crop Top",
+    price: "849₺",
+    category: "crop",
+    image: "assets/images/products/photo_5823488260943498610_y.jpg",
+    hoverImage: "assets/images/products/photo_5823488260943498609_y.jpg",
+    whatsappText: "VÉRAXA Black Crop Top"
+  },
+  {
+    name: "VÉRAXA Beige Crop Top",
+    price: "849₺",
+    category: "crop",
+    image: "assets/images/products/photo_5823488260943498622_y.jpg",
+    hoverImage: "assets/images/products/photo_5823488260943498623_y.jpg",
+    whatsappText: "VÉRAXA Beige Crop Top"
+  },
+  {
+    name: "VÉRAXA Burgundy Crop Top",
+    price: "849₺",
+    category: "crop",
+    image: "assets/images/products/photo_5823582883367995777_y.jpg",
+    hoverImage: "assets/images/products/photo_5823582883367995778_y.jpg",
+    whatsappText: "VÉRAXA Burgundy Crop Top"
   }
 ];
