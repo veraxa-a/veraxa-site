71c7cb2c514fdc3c64b5ed1786b64621c3b49b50
veraxa-a <veraxa.contact@gmail.com>
2026-05-19 01:38:42 +0300

    Add product details to VERAXA_PRODUCTS array

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,1 +1,23 @@
-const VERAXA_PRODUCTS = [];
+const VERAXA_PRODUCTS = [
+  {
+    name: "VÉRAXA Oversize Tee",
+    price: "1.290₺",
+    category: "tshirt",
+    image: "assets/images/products/product-1.jpg",
+    whatsappText: "VÉRAXA Oversize Tee"
+  },
+  {
+    name: "VÉRAXA Shadow Hoodie",
+    price: "2.490₺",
+    category: "hoodie",
+    image: "assets/images/products/product-2.jpg",
+    whatsappText: "VÉRAXA Shadow Hoodie"
+  },
+  {
+    name: "VÉRAXA Essential Black",
+    price: "1.590₺",
+    category: "tshirt",
+    image: "assets/images/products/product-3.jpg",
+    whatsappText: "VÉRAXA Essential Black"
+  }
+];
