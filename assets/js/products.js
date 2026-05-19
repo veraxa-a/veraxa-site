6af37c6a95767f9b710b5b1e3a38e79c0ed9cbb7
veraxa-a <veraxa.contact@gmail.com>
2026-05-19 04:57:30 +0300

    Update product list and images in products.js
    Removed several product entries and updated images and descriptions for existing products.

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,112 +1,183 @@
 window.VERAXA_PRODUCTS = [
   {
-    id: "fitilli-atlet",
     name: "VÉRAXA Fitilli Atlet Koleksiyonu",
     price: "₺ 699.00",
     category: "Kadın",
     image: "assets/images/products/01 (1).jpg",
     fallbackImage: "assets/images/products/fitilli-atlet-koleksiyonu/01.jpg",
-    images: ["assets/images/products/01 (1).jpg", "assets/images/products/02 (2).jpg", "assets/images/products/01 (5).jpg", "assets/images/products/02 (6).jpg"],
-    fallbackImages: ["assets/images/products/fitilli-atlet-koleksiyonu/01.jpg", "assets/images/products/fitilli-atlet-koleksiyonu/02.jpg", "assets/images/products/fitilli-atlet-koleksiyonu/03.jpg", "assets/images/products/fitilli-atlet-koleksiyonu/04.jpg"],
+    images: [
+      "assets/images/products/01 (1).jpg",
+      "assets/images/products/02 (2).jpg",
+      "assets/images/products/01 (5).jpg",
+      "assets/images/products/02 (6).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/fitilli-atlet-koleksiyonu/01.jpg",
+      "assets/images/products/fitilli-atlet-koleksiyonu/02.jpg",
+      "assets/images/products/fitilli-atlet-koleksiyonu/03.jpg",
+      "assets/images/products/fitilli-atlet-koleksiyonu/04.jpg"
+    ],
     description: "Fitilli dokulu premium atlet koleksiyonu."
   },
   {
-    id: "crop-basic",
     name: "VÉRAXA Crop Basic Koleksiyonu",
     price: "₺ 749.00",
     category: "Kadın",
     image: "assets/images/products/01 (29).jpg",
     fallbackImage: "assets/images/products/crop-basic-koleksiyonu/01.jpg",
-    images: ["assets/images/products/01 (29).jpg", "assets/images/products/02 (30).jpg", "assets/images/products/03 (31).jpg", "assets/images/products/04 (32).jpg"],
-    fallbackImages: ["assets/images/products/crop-basic-koleksiyonu/01.jpg", "assets/images/products/crop-basic-koleksiyonu/02.jpg", "assets/images/products/crop-basic-koleksiyonu/03.jpg", "assets/images/products/crop-basic-koleksiyonu/04.jpg"],
-    description: "Yumusak dokulu crop basic ust koleksiyonu."
+    images: [
+      "assets/images/products/01 (29).jpg",
+      "assets/images/products/02 (30).jpg",
+      "assets/images/products/03 (31).jpg",
+      "assets/images/products/04 (32).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/crop-basic-koleksiyonu/01.jpg",
+      "assets/images/products/crop-basic-koleksiyonu/02.jpg",
+      "assets/images/products/crop-basic-koleksiyonu/03.jpg",
+      "assets/images/products/crop-basic-koleksiyonu/04.jpg"
+    ],
+    description: "Yumuşak dokulu crop basic üst koleksiyonu."
   },
   {
-    id: "oversize-basic",
     name: "VÉRAXA Oversize Basic Tee",
     price: "₺ 899.00",
     category: "Unisex",
     image: "assets/images/products/01 (45).jpg",
     fallbackImage: "assets/images/products/oversize-basic-tee/01.jpg",
-    images: ["assets/images/products/01 (45).jpg", "assets/images/products/02 (46).jpg", "assets/images/products/03 (47).jpg", "assets/images/products/04 (48).jpg"],
-    fallbackImages: ["assets/images/products/oversize-basic-tee/01.jpg", "assets/images/products/oversize-basic-tee/02.jpg", "assets/images/products/oversize-basic-tee/03.jpg", "assets/images/products/oversize-basic-tee/04.jpg"],
+    images: [
+      "assets/images/products/01 (45).jpg",
+      "assets/images/products/02 (46).jpg",
+      "assets/images/products/03 (47).jpg",
+      "assets/images/products/04 (48).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/oversize-basic-tee/01.jpg",
+      "assets/images/products/oversize-basic-tee/02.jpg",
+      "assets/images/products/oversize-basic-tee/03.jpg",
+      "assets/images/products/oversize-basic-tee/04.jpg"
+    ],
     description: "Modern kesim premium oversize basic tee."
   },
   {
-    id: "cizgili-polo",
     name: "VÉRAXA Çizgili Polo Koleksiyonu",
     price: "₺ 849.00",
     category: "Kadın",
     image: "assets/images/products/01 (49).jpg",
     fallbackImage: "assets/images/products/cizgili-polo-koleksiyonu/01.jpg",
-    images: ["assets/images/products/01 (49).jpg", "assets/images/products/02 (50).jpg", "assets/images/products/03 (51).jpg", "assets/images/products/04 (52).jpg"],
-    fallbackImages: ["assets/images/products/cizgili-polo-koleksiyonu/01.jpg", "assets/images/products/cizgili-polo-koleksiyonu/02.jpg", "assets/images/products/cizgili-polo-koleksiyonu/03.jpg", "assets/images/products/cizgili-polo-koleksiyonu/04.jpg"],
-    description: "Zamansiz cizgili polo formu."
+    images: [
+      "assets/images/products/01 (49).jpg",
+      "assets/images/products/02 (50).jpg",
+      "assets/images/products/03 (51).jpg",
+      "assets/images/products/04 (52).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/cizgili-polo-koleksiyonu/01.jpg",
+      "assets/images/products/cizgili-polo-koleksiyonu/02.jpg",
+      "assets/images/products/cizgili-polo-koleksiyonu/03.jpg",
+      "assets/images/products/cizgili-polo-koleksiyonu/04.jpg"
+    ],
+    description: "Zamansız çizgili polo formu."
   },
   {
-    id: "oversize-blouse",
     name: "VÉRAXA Oversize Blouse",
     price: "₺ 999.00",
     category: "Kadın",
     image: "assets/images/products/01 (63).jpg",
     fallbackImage: "assets/images/products/oversize-blouse/01.jpg",
-    images: ["assets/images/products/01 (63).jpg", "assets/images/products/02 (64).jpg", "assets/images/products/03 (65).jpg", "assets/images/products/04 (66).jpg"],
-    fallbackImages: ["assets/images/products/oversize-blouse/01.jpg", "assets/images/products/oversize-blouse/02.jpg", "assets/images/products/oversize-blouse/03.jpg", "assets/images/products/oversize-blouse/04.jpg"],
-    description: "Dokumlu ve rahat oversize blouse."
+    images: [
+      "assets/images/products/01 (63).jpg",
+      "assets/images/products/02 (64).jpg",
+      "assets/images/products/03 (65).jpg",
+      "assets/images/products/04 (66).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/oversize-blouse/01.jpg",
+      "assets/images/products/oversize-blouse/02.jpg",
+      "assets/images/products/oversize-blouse/03.jpg",
+      "assets/images/products/oversize-blouse/04.jpg"
+    ],
+    description: "Dökümlü ve rahat oversize blouse."
   },
   {
-    id: "oversize-striped",
     name: "VÉRAXA Oversize Striped Tee",
     price: "₺ 899.00",
     category: "Kadın",
     image: "assets/images/products/01 (71).jpg",
     fallbackImage: "assets/images/products/oversize-striped-tee/01.jpg",
-    images: ["assets/images/products/01 (71).jpg", "assets/images/products/02 (72).jpg", "assets/images/products/03 (73).jpg", "assets/images/products/04 (74).jpg"],
-    fallbackImages: ["assets/images/products/oversize-striped-tee/01.jpg", "assets/images/products/oversize-striped-tee/02.jpg", "assets/images/products/oversize-striped-tee/03.jpg", "assets/images/products/oversize-striped-tee/04.jpg"],
-    description: "Oversize cizgili tisort koleksiyonu."
+    images: [
+      "assets/images/products/01 (71).jpg",
+      "assets/images/products/02 (72).jpg",
+      "assets/images/products/03 (73).jpg",
+      "assets/images/products/04 (74).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/oversize-striped-tee/01.jpg",
+      "assets/images/products/oversize-striped-tee/02.jpg",
+      "assets/images/products/oversize-striped-tee/03.jpg",
+      "assets/images/products/oversize-striped-tee/04.jpg"
+    ],
+    description: "Oversize çizgili tişört koleksiyonu."
   },
   {
-    id: "blue-midi",
     name: "VÉRAXA Blue Midi Dress",
     price: "₺ 1,299.00",
     category: "Kadın",
     image: "assets/images/products/01 (95).jpg",
     fallbackImage: "assets/images/products/blue-midi-dress/01.jpg",
-    images: ["assets/images/products/01 (95).jpg", "assets/images/products/02 (96).jpg", "assets/images/products/03 (97).jpg", "assets/images/products/04 (98).jpg"],
-    fallbackImages: ["assets/images/products/blue-midi-dress/01.jpg", "assets/images/products/blue-midi-dress/02.jpg", "assets/images/products/blue-midi-dress/03.jpg", "assets/images/products/blue-midi-dress/04.jpg"],
+    images: [
+      "assets/images/products/01 (95).jpg",
+      "assets/images/products/02 (96).jpg",
+      "assets/images/products/03 (97).jpg",
+      "assets/images/products/04 (98).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/blue-midi-dress/01.jpg",
+      "assets/images/products/blue-midi-dress/02.jpg",
+      "assets/images/products/blue-midi-dress/03.jpg",
+      "assets/images/products/blue-midi-dress/04.jpg"
+    ],
     description: "Mavi midi elbise."
   },
   {
-    id: "black-polo-dress",
     name: "VÉRAXA Black Polo Dress",
     price: "₺ 1,199.00",
     category: "Kadın",
     image: "assets/images/products/01 (103).jpg",
     fallbackImage: "assets/images/products/black-polo-dress/01.jpg",
-    images: ["assets/images/products/01 (103).jpg", "assets/images/products/02 (104).jpg", "assets/images/products/03 (105).jpg", "assets/images/products/04 (106).jpg"],
-    fallbackImages: ["assets/images/products/black-polo-dress/01.jpg", "assets/images/products/black-polo-dress/02.jpg", "assets/images/products/black-polo-dress/03.jpg", "assets/images/products/black-polo-dress/04.jpg"],
+    images: [
+      "assets/images/products/01 (103).jpg",
+      "assets/images/products/02 (104).jpg",
+      "assets/images/products/03 (105).jpg",
+      "assets/images/products/04 (106).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/black-polo-dress/01.jpg",
+      "assets/images/products/black-polo-dress/02.jpg",
+      "assets/images/products/black-polo-dress/03.jpg",
+      "assets/images/products/black-polo-dress/04.jpg"
+    ],
     description: "Siyah polo elbise."
   },
   {
-    id: "grey-set",
     name: "VÉRAXA Grey Two Piece Set",
     price: "₺ 1,699.00",
     category: "Kadın",
     image: "assets/images/products/01 (107).jpg",
     fallbackImage: "assets/images/products/grey-two-piece-set/01.jpg",
-    images: ["assets/images/products/01 (107).jpg", "assets/images/products/02 (108).jpg", "assets/images/products/03 (109).jpg", "assets/images/products/04 (110).jpg"],
-    fallbackImages: ["assets/images/products/grey-two-piece-set/01.jpg", "assets/images/products/grey-two-piece-set/02.jpg", "assets/images/products/grey-two-piece-set/03.jpg", "assets/images/products/grey-two-piece-set/04.jpg"],
-    description: "Gri ikili takim."
+    images: [
+      "assets/images/products/01 (107).jpg",
+      "assets/images/products/02 (108).jpg",
+      "assets/images/products/03 (109).jpg",
+      "assets/images/products/04 (110).jpg"
+    ],
+    fallbackImages: [
+      "assets/images/products/grey-two-piece-set/01.jpg",
+      "assets/images/products/grey-two-piece-set/02.jpg",
+      "assets/images/products/grey-two-piece-set/03.jpg",
+      "assets/images/products/grey-two-piece-set/04.jpg"
+    ],
+    description: "Gri ikili takım."
   },
   {
-    id: "navy-set",
-    name: "VÉRAXA Navy Two Piece Set",
-    price: "₺ 1,699.00",
-    category: "Kadın",
-    image: "assets/images/products/01 (111).jpg",
-    fallbackImage: "assets/images/products/navy-two-piece-set/01.jpg",
-    images: ["assets/images/products/01 (111).jpg", "assets/images/products/02 (112).jpg", "assets/images/products/03 (113).jpg", "assets/images/products/04 (114).jpg"],
-    fallbackImages: ["assets/images/products/navy-two-piece-set/01.jpg", "assets/images/products/navy-two-piece-set/02.jpg", "assets/images/products/navy-two-piece-set/03.jpg", "assets/images/products/navy-two-piece-set/04.jpg"],
-    description: "Lacivert ikili takim."
-  }
-];
+    name:
