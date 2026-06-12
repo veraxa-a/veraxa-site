19a5370fa81f3e26b68b356380c902fe81375e04
veraxa-a <veraxa.contact@gmail.com>
2026-06-12 14:46:18 +0300

    Add Veraxa affiliate product picks

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,18 +1,75 @@
 const products = [
   {
-    name: "Urun 1",
-    description: "Ilk komisyonlu satis linkin icin yer tutucu.",
-    url: "#"
+    name: "Siyah cicek broş toka",
+    category: "Aksesuar",
+    description: "Broş trendine uygun, sac veya kiyafet uzerinde kullanilabilen vintage gorunumlu parca.",
+    price: "$4.68",
+    source: "Pinterest trend",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012458629528.html%253Fpdp_npi%253D6%252540dis%252521USD%25252131.18%2525214.68%252521%252521%252521210.23%25252131.53%252521%25254021013cc617812205158737619e1125%25252112000058460392413%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/Sb8699b75dc444189b32965ae1b0ab645R.jpg"
   },
   {
-    name: "Urun 2",
-    description: "Platformdan aldigin satis ortakligi linkini buraya ekle.",
-    url: "#"
+    name: "Isisiz sac bukle seti",
+    category: "Guzellik",
+    description: "Saten boneli ve tokali set; isi kullanmadan dogal dalga isteyenler icin.",
+    price: "$7.92",
+    source: "Beauty trend",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012411299861.html%253Fpdp_npi%253D6%252540dis%252521USD%25252152.79%2525217.92%252521%252521%252521355.98%25252153.40%252521%2525402140f88817812204638917072e14bf%25252112000058332430317%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S00d8824401f5446d951156de5cbd633eT.jpg"
   },
   {
-    name: "Urun 3",
-    description: "Kisa, net ve guven veren urun aciklamasi kullan.",
-    url: "#"
+    name: "Dalga formlu cam pipet",
+    category: "Mutfak",
+    description: "Icecek sunumu icin tekrar kullanilabilir, renkli cam pipet seti.",
+    price: "$6.23",
+    source: "Google trend",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005008740403923.html%253Fpdp_npi%253D6%252540dis%252521USD%25252112.46%2525216.23%252521%252521%25252184.01%25252142.00%252521%2525402151fce817812189381644138e0fcf%25252112000046542617687%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S74a1b12b50724ad2bbdb8bc7375784f8H.jpg"
+  },
+  {
+    name: "Gold baharat standi",
+    category: "Mutfak",
+    description: "Uc bolmeli, standli ve duzenli mutfak kullanimi icin secildi.",
+    price: "$5.87",
+    source: "Home edit",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012368743979.html%253Fpdp_npi%253D6%252540dis%252521USD%25252173.43%2525215.87%252521%252521%252521495.17%25252139.61%252521%2525402141149b17812191969257726e0fcd%25252112000058193789148%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S6639f572b72c4e018dfe589594f356b9n.jpg"
+  },
+  {
+    name: "Dijital bagaj tartisi",
+    category: "Seyahat",
+    description: "Kompakt LCD ekranli valiz tartisi; seyahat oncesi pratik kontrol.",
+    price: "$4.27",
+    source: "Travel pick",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012454409679.html%253Fpdp_npi%253D6%252540dis%252521USD%25252161.04%2525214.27%252521%252521%252521411.61%25252128.81%252521%2525402101611117812200642767017e0e3d%25252112000058445166137%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S7a014c9f0093440b8388fc38d8e30758v.jpg"
+  },
+  {
+    name: "Katlanir spor seyahat cantasi",
+    category: "Canta",
+    description: "Spor, yuzme ve kisa seyahatler icin ayakkabi bolmeli hafif canta.",
+    price: "$7.51",
+    source: "Sporty trend",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012304689261.html%253Fpdp_npi%253D6%252540dis%252521USD%25252137.51%2525217.51%252521%252521%252521252.90%25252150.58%252521%25254021013cc617812198753467634e0eb1%25252112000058042514503%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S9a380f5bf40c46f1b2a55e8cea021f66U.jpg"
+  },
+  {
+    name: "A4 dosya organizeri",
+    category: "Ofis",
+    description: "Belgeler, okul ve evrak duzeni icin 100 sayfalik seffaf klasor.",
+    price: "$4.17",
+    source: "Office pick",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012403672692.html%253Fpdp_npi%253D6%252540dis%252521USD%25252159.59%2525214.17%252521%252521%252521401.82%25252128.13%252521%2525402151fce817812212093754058e10e5%25252112000058301176355%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/Sf2324ea56ac94e76b6e4fdc9a5e3cf617.jpg"
+  },
+  {
+    name: "Geometrik kupe seti",
+    category: "Aksesuar",
+    description: "Gunluk kombinler icin coklu ve sade geometrik kupe secimi.",
+    price: "$5.00",
+    source: "Accessory trend",
+    url: "https://rzekl.com/g/1e8d114494ded226ef3416525dc3e8/?ulp=https%3A%2F%2Fs.click.aliexpress.com%2Fdeep_link.htm%3Faff_short_key%3D_c3s1yQkJ%26dl_target_url%3Dhttps%253A%252F%252Fwww.aliexpress.com%252Fitem%252F1005012299275333.html%253Fpdp_npi%253D6%252540dis%252521USD%25252133.30%2525215.00%252521%252521%252521224.52%25252133.68%252521%2525402151fd3a17812208736442682e117a%25252112000058025924470%252521affd%252521%252521%252521%2525211%2525210%252521%26hot_product%3D0&i=5&f_id=50002",
+    image: "https://images.weserv.nl/?url=ae-pic-a1.aliexpress-media.com/kf/S62230b1156df40d2bbf23a3283bcfeedA.jpg"
   }
 ];
 
@@ -20,11 +77,20 @@ const grid = document.querySelector("#productGrid");
 
 grid.innerHTML = products.map((product) => `
   <article class="product-card">
-    <div class="media" aria-hidden="true"></div>
-    <div>
+    <a class="product-media" href="${product.url}" target="_blank" rel="nofollow sponsored noopener" aria-label="${product.name}">
+      <img src="${product.image}" alt="${product.name}" loading="lazy">
+    </a>
+    <div class="product-body">
+      <div class="product-meta">
+        <span>${product.category}</span>
+        <span>${product.source}</span>
+      </div>
       <h3>${product.name}</h3>
       <p>${product.description}</p>
     </div>
-    <a class="button primary" href="${product.url}" target="_blank" rel="nofollow sponsored noopener">Incele</a>
+    <div class="product-action">
+      <strong>${product.price}</strong>
+      <a class="button primary" href="${product.url}" target="_blank" rel="nofollow sponsored noopener">Incele</a>
+    </div>
   </article>
 `).join("");
